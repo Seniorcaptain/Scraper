87f6b7367682e8950297d13b4d02774f5e98a79e
Seniorcaptain <36276653+Seniorcaptain@users.noreply.github.com>
2021-11-16 04:55:22 +0000

    Add files via upload

--- counter.py
+++ counter.py
@@ -1,21 +1,6 @@
 import string
 
-Text = """'StanChart Q3 net profit jumps to Sh6.4bn
-1,StanChart Q3 net profit jumps to Sh6.4bn
-2,NMS steps up collection of house data
-3,"Companies World Bank, AfDB blacklist nearly 20 Kenya firms over fraud"
-4,Companies Safaricom debt rises five times to Sh76bn
-5,Companies State blocks Kenya Power from hiring McKinsey as adviser
-6,Economy Oil dealers get billions to keep fuel prices unchanged
-7,"Corporate University of Nairobi, KU losses hit Sh4.3 billion"
-8,News Blow to jobless Kenyan nurses as UK halts recruitment
-9,Corporate INTERVIEW: Why KRA is eyeing the nice things you post on Facebook
-10,Profiles On propelling a family’s legacy
-11,Economy Homes pay Sh5bn more for electricity theft in State deal
-12,Art Hybrid art auction earns Sh23 million
-13,DCI offers Sh60m in hunt for 3 terror suspects who escaped Kamiti
-14,Retail chain Mulleys shuts half of its outlets
-15,KQ adds Ethiopia flights as workers flee Tigray fight'"""
+filepath = 'Headlines.csv'
 word_count = {}
 
 
@@ -25,16 +10,17 @@ def strip_punctiations(line):
         return line
 
 
-#with open(Text, 'r') as fi:
+with open(filepath, 'r') as fi:
     # for each line in file
-    #for line in fi:
-      #  words = line.split()
+    for line in fi:
+        line = strip_punctiations(line)
+        words = line.split()
 
     # Cleaning text and lower casing all words
 for char in '-.,\n':
-    Text = Text.replace(char, ' ')
-Text = Text.lower()  # split returns a list of words delimited by sequences of whitespace (including tabs, newlines, etc, like re's \s)
-word_list = Text.split()
+    filepath = filepath.replace(char, ' ')
+filepath = filepath.lower()  # split returns a list of words delimited by sequences of whitespace (including tabs, newlines, etc, like re's \s)
+word_list = filepath.split()
 
 from collections import Counter
 Counter(word_list).most_common()
